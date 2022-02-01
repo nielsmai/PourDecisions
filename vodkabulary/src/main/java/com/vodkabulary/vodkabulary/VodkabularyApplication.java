836fa8835e08bd02ef272b332f7976abeea2b8b1
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-02-01 00:02:15 -0500

    Spring and Gradle initialization
    Spring Boot and Gradle initialization for the backend.

---
+++ vodkabulary/src/main/java/com/vodkabulary/vodkabulary/VodkabularyApplication.java
@@ -0,0 +1,20 @@
+package com.vodkabulary.vodkabulary;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class VodkabularyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodkabularyApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String greeting(){
+	  return "Hello world!";
+	}
+
+}
