250e0804ae208d80d6a730aa0fb98790a6c8510d
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-02-03 00:54:05 -0500

    Update VodkabularyApplicationTests.java

--- Vodkabulary-Backend/src/test/java/com/vodkabulary/VodkabularyApplicationTests.java
+++ Vodkabulary-Backend/src/test/java/com/vodkabulary/VodkabularyApplicationTests.java
@@ -1,12 +1,12 @@
 package com.vodkabulary;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class VodkabularyApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
