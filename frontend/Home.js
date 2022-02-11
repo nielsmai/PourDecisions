8accc8fbd1f90ae6edef922b213012e775ff9674
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-02-11 14:23:06 -0500

    Skeleton of the home page

---
+++ frontend/Home.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Home">
+        <body>Have a bunch of ingredients, but no idea what dirnks to make with them?</body>
+        <body>Don't worry and let us make your pour decisions.</body>
+        <body>Click on mix to begin!</body>
+        <button type="button">Mix!</button>
+    </div>
+  );
+}
+
+export default App;
