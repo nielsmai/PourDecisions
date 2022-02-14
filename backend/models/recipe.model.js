dd7317e74f8c9752a68b1cbb80283c818160bad6
Samuel Lin <lin.sam217@gmail.com>
2022-02-14 15:28:59 -0500

    Fixed model files

--- backend/models/recipe.model.js
+++ backend/models/recipe.model.js
@@ -1,14 +1,19 @@
-import mongoose from 'mongoose';
-
+// import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const {ingredientSchema} = require('./ingredient.model.js');
 var Schema = mongoose.Schema;
 
+// var Schema, assuming that content of Recipe can be changed
 var recipeSchema = new Schema({
-    garnish: { type: [String] },
-    instruction: { type: String, required: true },
+    ingredients: [ingredientSchema],    // Imported from ingredient.model.js
+    garnish: [{ type: String }],        // ingredient with type garnish instead?
+    instruction: { 
+        type: String, 
+        required: true },
 }, {
     timestamps: true,
 });
 
 // module.exports = mongoose.model('recipe', recipeSchema);
 var recipe = mongoose.model('Recipe', recipeSchema);
-export default recipe; 
+module.exports = { recipe }; 
