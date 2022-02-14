5960cad287755a25060b5f2a64812e5899396431
brimless <james.chaumt@gmail.com>
2022-02-14 15:55:16 -0500

    oops forgot to add path variable

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-// const path = require('path');
+const path = require('path');
 
 const usersRouter = require('./routes/users');
 const drinksRouter = require('./routes/drinks');
