6bad1d77f004ed112f54bdbdb893fb2adad1e05e
vy-khahuynh <77684345+vy-Khahuynh@users.noreply.github.com>
2022-02-13 20:23:52 -0500

    Fixed javascript and css file, added back arrow

--- frontend/src/components/changePasswordAccount/changePasswordAccount.js
+++ frontend/src/components/changePasswordAccount/changePasswordAccount.js
@@ -1,69 +1,63 @@
-import React, { useState } from 'react';
-
-export default function changePasswordAccount() {
+import './changePasswordAccount.css'
+import './martini.png'
+import './arrow.png'
+export default function ChangePasswordAccount() {
 
     return (
+        <>
+            <body>
+                <h1>Enter the following information below</h1>
+                
+                <div>
 
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+                <img src={require('./martini.png')} id="martini" alt="Martini Glass Logo" title="Martini Glass"width="400px"></img>
 
-<body>
-       <div class="topnav">
-            <b>PourDecisions</b>
-            <a>Mix</a>
-            <a>Drinks</a>
-            <a>Search</a>
-            <a>Login</a>
-        </div>
+                <table>
+                    <tr>
+                        <th>
+                            <p ><b>Username</b></p>
+                        </th>
+                        <td >
+                            <p class="dark" ><b>Anika</b></p>
+                        </td>
+                    </tr>
 
-<header class="dark">
-<h1>Enter the following information below</h1>
-</header>
+                    <tr>
+                        <th>
+                            <p ><b>Password</b></p>
+                        </th>
+                        <td >
+                            <form>
+                                <input v-model="oldPass" placeholder="Old Password"></input>
+                                <input v-model="newPass" placeholder="New Password"></input>
+                                <input v-model="confirmPass" placeholder="Confirm New Password"></input>
+                            </form>
+                        </td>
+                    </tr>
 
+                    <tr>
+                        <th>
+                            <p ><b>Email</b></p>
+                        </th>
+                        <td >
+                            <p class="dark" ><b>anika@gmail.com</b></p>
+                        </td>
+                    </tr>
+                </table>
+                </div>
 
-<div>
-
-<img src="./martini.png" title="Martini Glass" style="float:right" width=500px>
-
-<table>
-<tr>
-<th>
-<p style="font-size:25px; color: #7E003E;"><b>Username</b></p>
-</th>
-<td style="padding-left:200px;text-align:center;">
-<p class="dark" style="font-size:25px"><b>Anika</b></p>
-</td>
-</tr>
-
-<tr>
-<th>
-<p style="font-size:25px; color: #7E003E;"><b>Password</b></p>
-</th>
-<td style="padding-left: 200px">
-<input v-model="oldPass" placeholder="Old Password">
-<input v-model="newPass" placeholder="New Password">
-<input v-model="confirmPass" placeholder="Confirm New Password">
-</td>
-</tr>
-
-<tr>
-<th>
-<p style="font-size:25px; color: #7E003E;"><b>Email</b></p>
-</th>
-<td style="padding-left:200px; text-align:center;">
-<p class="dark" style="font-size:25px"><b>anika@gmail.com</b></p>
-</td>
-</tr>
-</table>
-</div>
-<div style="text-align:center;padding-left:160px">
-<a class = "button">Apply Changes</a>
-</div>
-
-</body>
-
-<link rel="stylesheet" href="changePasswordAccount.css">
-
-);
+                <div >
+                    <a class = "button" href="http://localhost:3000">Apply Changes</a>
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <div>
+                    <a href="http://localhost:3000">
+                    <img src={require('./arrow.png')} id="back" alt="Left arrow" title="Back"width="50px"></img>
+                    </a>
+                </div>
+            </body>
+        </>
+    );
 }
