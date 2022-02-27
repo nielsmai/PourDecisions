1ebdc8402aba8ec346895893fe5c2ea5d7b67566
AnikaKabir01 <anika.kabir@mail.mcgill.ca>
2022-02-27 17:38:16 -0500

    getAllDrinks hardcoded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
     // </div>
 
     getAllDrinks()
-    
+    //CreateDrink()
   );
 }
 
--- frontend/src/components/getAllDrinks/getAllDrinks.js
+++ frontend/src/components/getAllDrinks/getAllDrinks.js
@@ -6,12 +6,9 @@ export default function getAllDrinks() {
     return (
 
     <div>
-        <div class="searchBar" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh'}}>
+        <div class="searchBar" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '20vh'}}>
            
             <form action="/" method="get">
-            <label htmlFor="header-search">
-                <span className="visually-hidden">Search</span>
-            </label>
             <input
                 type="text"
                 id="header-search"
@@ -22,13 +19,41 @@ export default function getAllDrinks() {
             </form>
         </div>
 
-        <div class="filters" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '0vh'}}>
+        {/* <div class="filters" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '0vh'}}>
+            <label htmlFor="header-search">
+                <span className="visually-hidden">Enter an ingredient list, the name of drink or a creator to search for a drink</span>
+            </label>
+        </div> */}
+
+        <div class="filters" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '10vh'}}>
             <button type="button" class="filters">Alcoholic</button>
             <button type="button" class="filters">Mocktail</button>
             <button type="button" class="filters">Custom</button>
             <button type="button" class="filters">Newest</button>
             <button type="button" class="filters">Popularity</button>
         </div>  
+
+         <div>
+
+        <table>
+            <tr>
+                <th scope='col'>Ratings</th>
+                <th scope='col' >Name</th>
+                <th scope='col' >Ingredients</th>
+                <th scope='col' >Tags</th>
+            </tr>
+
+           
+            <tr>
+                <td >58</td>
+                <td > Easy Whiskey Sour</td>
+                <td >Ingredients: Bourbon, Lemon juice, Syrup</td>
+                <td >Alcoholic</td>
+                <a class = "buttonOther" href="http://localhost:3000"> View Details </a>
+             </tr>
+
+        </table>
+        </div>
                
     </div>
     );
