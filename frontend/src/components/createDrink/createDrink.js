33f87edeac0fce49d7242137d3e5dfaee915c2b9
Jericho <72528595+jericho-ad@users.noreply.github.com>
2022-03-10 04:11:21 -0500

    fixed create drink

--- frontend/src/components/createDrink/createDrink.js
+++ frontend/src/components/createDrink/createDrink.js
@@ -13,11 +13,6 @@ var AXIOS = axios.create({
 })
 
 export default function CreateDrink() {
-
-    const [pub, setPublic] = useState(false)
-    const [rating, setRating] = useState(0)
-    const[ingredientData, setIngredientData] = useState([])
-
     const [drink, setDrink] = useState({
         name: "",
         author: "",
@@ -29,47 +24,104 @@ export default function CreateDrink() {
                 }
             ],
             garnish: "",
-            instruction: ""
+            instructions: ""
         }
     })
 
-    const getData = async () => {
-        const response = await AXIOS.get('/drinks/ingredients/all')
-        
-        console.log(response.data)
-        
-        setIngredientData(response.data)
-    }
+    const [ingredientList, setIngredientList] = useState([])
 
-    const [ingredient, setIngredient] = useState({
-        ingredientName:"",
-        ingredientType:""
+    const [ingredient, setIngredient] = useState("")
+
+    const [garnish, setGarnish] = useState({
+        list:""
     })
-    const ingredients = []
 
     const addIngredient = (e) => {
         e.preventDefault()
+        setIngredientList([
+            ...ingredientList,
+            {
+                ingredientName: ingredient,
+                ingredientType: "OTHER"
+            }
+        ])
+
         setDrink({
             ... drink,
             recipe :{
-                ingredients: [{
-                    ingredients
-                }]
+                ingredients: ingredientList
             }
         })
-        console.log(drink)
-        console.log(ingredient.ingredientName)
-        ingredients.push(ingredient)
-        console.log(ingredients)
+        setIngredient('')
     }
 
-    const onChangeIN = (e) => {
-        
-        setIngredient((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value
-        }))
+    // const onChangeIngredientName = (e) => {
+    //     setIngredient({
+    //         ...ingredient,
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
 
+    const onChangeRecipeGarnish = (e) => {
+        setGarnish({
+            ...garnish,
+            [e.target.name]: e.target.value
+        })
+
+        const garnishList = garnish.list.split(',')
+
+        setDrink({
+            ... drink,
+            recipe :{
+                ingredients: ingredientList,
+                garnish: garnishList
+            }
+        })
+    }
+
+    const onChangeRecipeInstructions = (e) => {
+        setDrink({
+            ...drink,
+            recipe: {
+                ...drink.recipe,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    const onChangeDrinkName = (e) => {
+        setDrink({
+            ...drink,
+            [e.target.name]: e.target.value,
+        })
+
+    }
+
+    const onChangeDrinkAuthor = (e) => {
+        setDrink({
+            ...drink,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    const addDrink = (e) => {
+        e.preventDefault()
+        
+        
+        const garnishList = garnish.list.split(',')
+
+        setDrink({
+            ... drink,
+            recipe :{
+                ingredients: ingredientList,
+                garnish: garnishList
+            }
+        })
+
+        AXIOS.post('/drinks/add', drink)
+        .then(res => console.log(res.data))
+
+        console.log(drink)
     }
 
     return (
@@ -79,57 +131,57 @@ export default function CreateDrink() {
                 <Link to="/drinks/ingredients">Add a missing ingredient</Link>
             </div>
         </nav>
-        {/* <div>
-            <h3>Ingredient list</h3>
-            <button onClick={getData}>Show ingredients</button>
-            {
-                ingredientData && ingredientData.map((ingredientData, index) =>
-                {
-                    const ingredientName = ingredientData.ingredientName
-                    const ingredientType = ingredientData.ingredientType
-                
-                
-                return (
-                    <div key={index}>
-                        <h4>{ingredientName} [{ingredientType}]</h4>
-                    </div>
-                );
-                })}
-        </div> */}
 
         <h2>Create Drink</h2>
         <h3>Step 1.</h3>
         <h3>Add the ingredients that are in your drink</h3>
 
-        <form onSubmit={ addIngredient } >
+        <form>
             <input type="text"
-                name="ingredientName"
+                name="ingredient"
                 placeholder='Ingredient'
-                value={ingredient.ingredientName}
-                onChange={onChangeIN}></input>
+                value={ingredient}
+                onChange={e => setIngredient(e.target.value)}></input>
             <br></br><br></br>
             <button onClick={addIngredient}>Add Ingredient</button>
         </form>
+
+        <ul>
+            {ingredientList.map(item => (
+                <li>{item.ingredientName}</li>
+            ))}
+        </ul>
 
         <h3>Step 2.</h3>
         <h3>Write the recipe, and make sure to include tags!</h3>
 
         <form>
         <input type="text"
-                name="drinkName"
-                placeholder="Drink Name">
-                </input>
+                name="name"
+                placeholder="Drink Name"
+                onChange={onChangeDrinkName}
+                ></input>
             <br></br>
+
         <input type="text"
-                name="garnish"
+                name="list"
+                value={garnish.list}
+                onChange={onChangeRecipeGarnish}
                 placeholder="Garnish"></input>
                 <br></br>
+
         <input type="text"
-                name="instructionList"
+                name="instructions"
                 placeholder='Instructions'
+                onChange={onChangeRecipeInstructions}
                 width='100%'
                 size='100%'></input>
-                <br></br>
+
+        <input type="text"
+                name="author"
+                placeholder="Drink Author"
+                onChange={onChangeDrinkAuthor}
+                ></input>                <br></br>
         <input type="checkbox" 
                 name="pub"
                 >
@@ -146,7 +198,7 @@ export default function CreateDrink() {
             </select>
             <br></br><br></br>
 
-            <input type="submit" value="Submit"></input>
+            <input type="button" onClick={ addDrink }value="Add Drink"></input>
         </form>
 
         </>
