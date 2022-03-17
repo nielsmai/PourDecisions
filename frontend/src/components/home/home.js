13e15e74ed1372812085e58763f15a9a853dc39c
Niels <77604434+ballofsunshine@users.noreply.github.com>
2022-03-17 00:17:21 -0400

    added logInAccount logo
    Added the logo for logInAccount
    Also added a logo with cropped blank spaces.

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './home.css'
 // import "@fontsource/montserrat"
 
-import logo from '../../images/logo.png'
+import logo from '../../images/logoCrop.png'
 
 export default function Home() {
     return (
         <div className="Home" class="center text">
-            <img src={logo} alt="Logo"></img>
+            <img src={logo} alt="Logo" class="resizeLogo"></img>
             <body>Have a bunch of ingredients, but no idea what drinks to make with them?</body>
             <body>Don't worry and let us make your pour decisions.</body>
             <body>Click on mix to begin!</body>
