b8ceaf78d6d0b3320ad2337b4ba1d5a50a3b86bf
Jericho <72528595+jericho-ad@users.noreply.github.com>
2022-03-17 06:41:05 -0400

    Match createdrink functionality to figma, navbar modifications

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -5,19 +5,28 @@ export default class Navbar extends Component {
     render() {
         return (
             <nav>
-                <Link to="/">PourDecisions</Link>
                 <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
+                <ul className="navbar">
+                    <li className="navbar-item-l">
+                        <Link to="/">PourDecisions</Link>
                     </li>
-                    <li className="navbar-item">
-                        <Link to="/drinks/create">Drinks</Link>
-                    </li>
-                    <li className="navbar-item">
+                    <li className="navbar-item-r">
                         <Link to="/account/login">Log In</Link>
                     </li>
+                    <li className="navbar-item-c">
+                        <Link to="/drinks/create">Create</Link>
+                    </li>
+                    <li className="navbar-item-c">
+                        <Link to="/drinks/">Search</Link>
+                    </li>
+                    <li className="navbar-item-c">
+                        <Link to="/drinks/">Drinks</Link>
+                    </li>
+                    <li className="navbar-item-c">
+                        <Link to="/drinks/">Mix</Link>
+                    </li>
                 </ul>
+                <br></br>
                 </div>
             </nav>
         );
