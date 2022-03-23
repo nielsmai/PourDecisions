a4c930940e01277887364bf324f5ca553434942f
brimless <james.chaumt@gmail.com>
2022-03-22 21:23:50 -0400

    clean comments

--- backend/routes/drinks.js
+++ backend/routes/drinks.js
@@ -22,14 +22,13 @@ const {
     removeIngredient,
     removeDrink,
     getAllIngredients,
-    getIngredientByName
+    // getIngredientByName
 
 } = require('../controllers/drinks');
 
 const router = express.Router();
 
 // this is supposed to get info once we go to root
-// router.get('/', getAllDrinks);
 router.get('/', (req, res) => {
     getAllDrinks(req, res);
 })
