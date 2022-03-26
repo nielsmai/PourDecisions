7b785ae70d4ecbc1f4ba0f2cc2b3e2e6a4cbb90d
vy-khahuynh <77684345+vy-Khahuynh@users.noreply.github.com>
2022-03-26 15:50:09 -0400

    Basic of change visibility checkbox

--- frontend/src/components/viewCustomDrink/viewCustomDrink.js
+++ frontend/src/components/viewCustomDrink/viewCustomDrink.js
@@ -1,7 +1,64 @@
 import React, { useState } from 'react';
+import AXIOS from "../../axios.config"
 //import './viewCustomDrink.css'
 
+
+// export default function ChangeVsibility(){
+//     const [isChecked, setIsChecked] = useState(true);
+    
+    
+//     const handleOnChange = () => {
+//         setIsChecked(!isChecked);
+//       };
+
+//     return (
+//         <div className="App">
+//             Select your pizza topping:
+//                 <div className="topping">
+//                     <input
+//                     type="checkbox"
+//                     id="topping"
+//                     name="topping"
+//                     value="Paneer"
+//                     checked={isChecked}
+//                     onChange={handleOnChange}
+//                     />
+//                     Paneer
+//                 </div>
+//             <div className="result">
+//                 Above checkbox is {isChecked ? "checked" : "un-checked"}.
+//             </div>
+//         </div>
+//   );
+// }
+
+
+
 export default function ViewCustomDrink() {
+    const [isChecked, setIsChecked] = useState(false);
+    const [userUsername, setUserUsername] = useState(window.localStorage.getItem('loggedUsername'));
+    const [drinkName, setDrinkName] = useState("");
+
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+        const PostData = async () => {
+
+            let res = await AXIOS.put('/'+userUsername+'/'+drinkName+'/update/status', {
+                // username: userUsername,
+                // password: userOldPass,
+                // newPassword: userNewPass,
+                // confirmNewPassword: userConfirmedPassword
+            })
+            .then(response => {
+                // setIsSubmitted(true);
+            })
+            .catch(e => {
+                console.log(e.response.data.message)
+            })
+        }
+        PostData()  
+    };
+      
 
     return (
         <div id="drink">
@@ -19,8 +76,12 @@ export default function ViewCustomDrink() {
                         <td><button class='modify'>Delete</button></td>
                         <td>                        
                             <form align="center">
-                                <input type="checkbox" id="visibility" name="visbility" value="public"></input>
-                                <label for="visibility">Public</label>
+                                <input type="checkbox" id="visibility" name="visbility" value="public"  checked={isChecked}
+                    onChange={handleOnChange}></input>
+                                <label for="visibility">Visbility</label>
+                                <div className="result">
+                                    Above drink is {isChecked ? "private" : "public"}.
+                                </div>
                             </form>
                         </td>
                     </tr>
