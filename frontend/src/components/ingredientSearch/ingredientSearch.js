6d5acb39e13aa6daf23f51d9c214fec617e24d2b
Quang Loc Tran <quang.l.tran@mail.mcgill.ca>
2022-03-27 17:22:07 -0400

    Setup for mix

--- frontend/src/components/ingredientSearch/ingredientSearch.js
+++ frontend/src/components/ingredientSearch/ingredientSearch.js
@@ -1,4 +1,3 @@
-import { Axios } from "axios";
 import { Component } from "react";
 import AXIOS from '../../axios.config'
 
@@ -9,23 +8,31 @@ export default class ingredientSearch extends Component {
         this.addIngredients = this.addIngredients.bind(this)
 
         this.state={
-            ingredients: [],
+            ingredients: new Set(),
             drinks: []
         }
     }
 
-    addIngredients = (e) => this.setState([this.state.ingredients,e])
-
+    addIngredients = (e) => this.setState([this.state.ingredients,e.target.value])
 
     componentDidMount(){
         AXIOS.get('/drinks/').then(res => {
             this.setState({drinks : res.data})
         })
+        console.log(this.state.drinks)
+        this.setState({ingredients : new Set(this.state.drinks.map(drink => drink.recipe.ingredients))})
+        console.log(this.state.ingredients)
     }
 
     render() {
         return (
           <div>
+            <select onChange={this.addIngredients}>
+                {[this.state.ingredients].map(ingredients => (
+                    <option key={ingredients.name} value={ingredients.name}>{ingredients.name}</option>
+                ))}
+            </select>
+
           </div>  
         )
     }
