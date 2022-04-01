c9671efd569a460bae02e3b31bfeccb01a0ae5c3
brimless <james.chaumt@gmail.com>
2022-04-01 00:42:42 -0400

    when did this even get here what in the what the what????????

--- frontend/src/components/viewDrink/viewDrink.js
+++ frontend/src/components/viewDrink/viewDrink.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './viewDrink.css'
 import AXIOS from '../../axios.config'
-import { Link, useParams } from 'react-router-dom'
-import { set } from 'mongoose'
+import { useParams } from 'react-router-dom'
 
 export default function ViewDrink() {
     let { drinkId } = useParams()
