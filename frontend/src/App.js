c7e5f16ed5406895b205fda968a30a68878d7fe4
Jericho <72528595+jericho-ad@users.noreply.github.com>
2022-04-01 04:48:35 -0400

    temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,7 +44,6 @@ function App() {
           <Route path="/account/logout" element={<LogOutAccount/>} />
           <Route path="/account/register" element = {<CreateAccount/>} />
           <Route path="/drinks/" element = {<ViewDrinks/>} />
-          <Route path="/drinks/t" element = {<GetAllDrinks/>} />
           <Route path="/drinks/mix" element = {<IngredientSearch/>} />
           <Route path="/account/drinks" element = {<ViewCustomDrink/>} />
           <Route path="/account/drinks/id/:drinkId" element = {<ViewDrink/>} />
